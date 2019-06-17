1b03427cad1a677f3deb6fae408326f2c6e1091d
Akili1999 <sluground@gmail.com>
2019-06-17 04:40:35 -0400

    progress

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('bacon burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('mushroom burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('cheese burger', false);
