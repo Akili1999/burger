6ebaaed4fc94dbd976c6f1903027dc827e86036e
Akili1999 <sluground@gmail.com>
2019-06-20 17:14:19 -0400

    not working, jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 var mysql = require("mysql"); // We require the extension of mysql
 // We use mysql to create a connection to local host on mysql port 3306
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ipod115s",
-  database: "burgers_db"
-});
-// We throw an error if the connection fails, or log the id of the connection if the connection is successful
-connection.connect(function(err){
-    if(err){
-        console.error("error connection: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ipod115s',
+    database: 'burgers_db'
+  });
+};
+
+connection.connect()
 // We export the connection for later use
 module.exports = connection;
